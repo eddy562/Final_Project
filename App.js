a6b9185b84a210efbd55797e29389f5a2ba0bddf
EddyM <Eddy.Mendoza01@student.csulb.edu>
2022-06-15 15:29:28 -0700

    CryptoTracker update

--- App.js
+++ App.js
@@ -1,58 +1,22 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Image } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+//import CoinItem from './src/components/CoinItem';
+import CoinItem from "./src/components/CoinItem";
 
+ 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.coinContainer}>
-        <Image
-          source={{
-            uri: "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579",
-          }}
-          style={{ 
-            height: 30, 
-            width: 30, 
-            marginRight: 10,
-            alignSelf:'center' }}
-        />
-
-        <View>
-          <Text style={styles.title}>Bitcoin</Text>
-          <View style={{ flexDirection: "row" }}>
-            <Text style={styles.text}>1</Text>
-            <Text style={styles.text}>BTC</Text>
-            <Text style={styles.text}>0.63%</Text>
-          </View>
-        </View>
-      </View>
-
+      <CoinItem/>
       <StatusBar style="light" />
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#121212",
     paddingTop: 50,
-    
-    // alignItems: 'center',
-    //justifyContent: 'center',
-  },
-
-  title: {
-    color: "white",
-    fontSize: 16,
-    fontWeight: "bold",
-  },
-  text: {
-    color: "white",
-  },
-
-  coinContainer:{
-    flexDirection: "row",
-    borderBottomWidth: StyleSheet.hairlineWidth,
-    borderBottomColor:'grey'
   },
 });
