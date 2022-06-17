8ba2e2e1a3a592d9d47a0cd7b3dfdd84365d053e
EddyM <Eddy.Mendoza01@student.csulb.edu>
2022-06-16 20:14:02 -0700

    crypto time code 2:28:50 working

--- src/screens/CoinDetailedScreen/index.jsx
+++ src/screens/CoinDetailedScreen/index.jsx
@@ -1,24 +1,31 @@
 import react from "react";
 
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, Dimensions } from "react-native";
 import { Ionicons, EvilIcons } from "@expo/vector-icons";
 import Coin from "../../../assets/data/crypto.json";
 import CoinDetailedHeader from "./components/CoinDetailedHeader";
-import styles from './styles';
-import {AntDesign} from "@expo/vector-icons";
+import styles from "./styles";
+import { AntDesign } from "@expo/vector-icons";
+/* import {
+  ChartDot,
+  ChartPath,
+  ChartPathProvider,
+} from "@rainbow-me/animated-charts"; */  // I think the issue is here 
 
 const CoinDetailedScreen = () => {
   const {
     image: { small },
     name,
     symbol,
-    market_data: { market_cap_rank, 
-        current_price,
-        price_change_percentage_24h,
-
-     },
+    prices,
+    market_data: {
+      market_cap_rank,
+      current_price,
+      price_change_percentage_24h,
+    },
   } = Coin;
-  const percentageColor = price_change_percentage_24h < 0 ? '#ea3943':'#16c784' 
+  const percentageColor = price_change_percentage_24h < 0 ? "#ea3943" : "#16c784";
+  //const screenWidth= Dimensions.get("window").width;
 
   return (
     <View style={{paddingHorizontal:10}}>
--- src/screens/CoinDetailedScreen/styles.js
+++ src/screens/CoinDetailedScreen/styles.js
@@ -1,40 +1,31 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-  currentPrice:{
+  currentPrice: {
+    color: "white",
+    fontSize: 30,
+    fontWeight: "600",
+    letterSpacing: 1,
+  },
+  name: {
+    color: "white",
+    fontSize: 15,
+  },
 
-        color:'white',
-         fontSize:30, 
-         fontWeight:'600', 
-         letterSpacing:1,
+  priceContainer: {
+    padding: 15,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
 
-         } ,
-  name:{
-
-    color:'white', 
-    fontSize:15.
-
-},
-
-priceContainer:
-    {padding:15, 
-        flexDirection:'row',
-         justifyContent:'space-between',
-           alignItems:'center', 
-        
-        },
-
-        priceChange:{
-
-            color:'white',
-             fontSize:17, 
-             fontWeight:'500 ',
-        }
-
-  
-
-
-
+  priceChange: {
+    color: "white",
+    //backgroundColor:"red",
+    //borderRadius:4,
+    fontSize: 17,
+    fontWeight: "500",
+  },
 });
 
 export default styles;
